c796fec387c5f4f249b2ebc24ac5b7d60d91d584
danielWatson3141 <danielwatson3141@gmail.com>
2019-01-16 16:23:43 -0500

    -Added Windows workable mining. -Added feature extractor prototypes. -Modified feature extraction code to allow for additional extractors

---
+++ featureExtractors.py
@@ -0,0 +1,23 @@
+from scipy.sparse import csr_matrix
+class featureExtractors:
+    #when a new feature is added, added its name to the matching list.
+    #order of the names matters, should match order in output vector
+    charfeatureNames = []
+    tokfeatureNames = []
+    astfeatureNames = []
+
+    featureNames = charfeatureNames + tokfeatureNames + astfeatureNames
+
+    @staticmethod
+    def characterLevel(function:str):
+        """ returns row vector in scipy.sparse.csr_matrix row format of features extracted from function string"""
+        return csr_matrix(0, shape = (1, 1))    #singleton 0 as default
+    @staticmethod
+    def tokenLevel(tokens):
+        """ Returns row vector scipy.sparse.csr_matrix of features extracted from token iterable"""
+        return csr_matrix(0, shape = (1, 1))    #singleton 0 as default
+
+    @staticmethod
+    def astLevel(ast):
+        """ Returns row vector scipy.sparse.csr_matrix of features extracted from ast"""
+        return csr_matrix(0, shape = (1, 1))    #singleton 0 as default
--- gitProfileSet.py
+++ gitProfileSet.py
@@ -14,7 +14,7 @@ class gitProfileSet:
 
     #TODO: Make it so re-compiling doesn't break
     #TODO: Make sibling class of ProfileSet
-    #TODO: Make multi-threaded
+    
     langList =["cpp", "c"]
 
     def __init__(self):
@@ -78,6 +78,80 @@ class gitProfileSet:
           
     
     def compileAuthors(self):
+        import sys
+        platform = sys.platform
+
+        if platform is "darwin" or platform is "linux":
+            print("Doing work in parellel")
+            self.compileInParellel()
+        else:
+            print("Doing work in sequence")
+            self.compileInSequence()
+
+    def compileInSequence(self):
+        for repo in self.repos:
+            miner = pydriller.repository_mining.RepositoryMining(repo, only_modifications_with_file_types=gitProfileSet.langList,only_no_merge=True)
+            repository = pydriller.GitRepository(repo)
+            print("Scanning repo: "+miner._path_to_repo)
+            
+            
+            tipeCounts = dict()
+
+            for commit in miner.traverse_commits():
+                tipe = commitType.categorize(commit)
+
+                if tipe not in tipeCounts:
+                    tipeCounts.update({tipe: 0})
+                count = tipeCounts.get(tipe)
+                tipeCounts.update({tipe: count+1})
+
+
+                if tipe is commitType.FEATURE:
+                    author = commit.author
+                    if author.name not in self.authors:
+                        self.authors.update({author.name:gitAuthor(author)})
+                        print("Found new author: "+author.name)
+                    
+                    author = self.authors.get(author.name)
+                    
+                    for mod in commit.modifications:
+                        mod._calculate_metrics()
+                        if mod.new_path is None or not mod.new_path.split(".")[-1] in gitProfileSet.langList:
+                            continue
+                        
+                        author.files.add(mod.new_path)
+                        #parse diff and add lines to list
+                        
+                        leDiff = repository.parse_diff(mod.diff)
+                        for num, line in leDiff["added"]:
+                            author.lines.update({(commit.hash,mod.new_path,num):line})
+
+                        #extract functions with lizard
+                        funs = mod._function_list
+
+                        #maintain list of dicts containing the source code of specific functions. Same format as for lines
+                        lineIndex = 0
+                        for fun in funs:
+                            
+                            try:
+                                while(leDiff["added"][lineIndex][0]<fun.start_line):
+                                    lineIndex+=1
+                            
+                                while(leDiff["added"][lineIndex][0]<fun.end_line):
+                                    author.functions.append({(commit.hash,mod.new_path,leDiff["added"][lineIndex][0]):leDiff["added"][lineIndex][1]})
+                                    lineIndex+=1
+                            except IndexError: #if end of input reached before end of functions. This is probable when non-complete functions are submitted.
+                                pass
+
+                    
+                    
+            print(str("finished"+str(miner._path_to_repo)))
+            print("types: "+str(tipeCounts))
+
+
+
+
+    def compileInParellel(self):
         print("Gathering Author data...")
         
         startTime = time.time()
@@ -197,13 +271,14 @@ class gitProfileSet:
         inputs=[]
         self.target = []
         print("Tokenizing...")
-        for author in self.authors.values():
-            for fun in author.functions:
-                inputs.append(PPTools.Tokenize.fun(fun))
-                self.target.append(author.name)
+        for author in self.authors.values():    #for each author
+            for fun in author.functions:        #Take each function
+                inputs.append(PPTools.Tokenize.fun(fun))    #append it to inputs
+                self.target.append(author.name)             #Author name is target
+
         print("Textifying...")
         for i in range(0,len(inputs)):
-            inputs[i] = PPTools.Tokenize.tokensToText(inputs[i])
+            inputs[i] = PPTools.Tokenize.tokensToText(inputs[i]) #Convert to text
 
         print("Vectorizing...")
         vectorizer =  TfidfVectorizer(analyzer="word", token_pattern="\S*", decode_error="ignore", lowercase=False)
@@ -215,8 +290,14 @@ class gitProfileSet:
             #should fit feature detector here
             #then pass it down
 
-    
+    def functionToString(self, lines):
+        textLines = list(len(lines))
 
+        for lineInfo, line in lines:
+            cHash, fil, lnum = lineInfo
+            textLines[lnum] = line
+
+        return "\n".join(textLines)
 class gitAuthor:
 
     def __init__(self, dev):
@@ -229,6 +310,7 @@ class gitAuthor:
         self.lines = dict() #key: {commitHash,file.cpp,lineNumber} value: literal code
         self.repos = set()
     
+    #TODO: Do I still need this?
     @classmethod
     def fromProxy(thisClass, proxy):
         toReturn = gitAuthor(None)
@@ -262,7 +344,6 @@ class gitAuthor:
         
     def __str__(self):
         return self.name+": "+str(len(self.commits.items()))+" commits. "+str(len(self.lines))+" LOC, "+str(len(self.functions))+" complete functions."
-    
 
 class gitInfo:
     """Container class for extracted git info"""
@@ -309,7 +390,7 @@ class gitInfo:
                         lineIndex+=1
                 except IndexError: #if end of input reached before end of functions. This is probable when non-complete functions are submitted.
                     pass
-
+    #TODO: clean unnecessary getters
     def getrepo(self):  
         return self.repo
     def gethash(self):  
