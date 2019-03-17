4febbec3aa87833daf3539e8613dc1bb5be39896
Jason Shau Ji PNg <jsjpng@uwaterloo.ca>
2019-03-16 20:50:36 -0400

    Added average ROC plots

--- plotting.py
+++ plotting.py
@@ -1,4 +1,7 @@
+import numpy as np
 import matplotlib.pyplot as plt
+from scipy import interp
+from sklearn.metrics import auc
 
 # Generates distinct colours given a number. Returns black if the index is too high
 def get_color(index):
@@ -9,6 +12,34 @@ def get_color(index):
     if (index < len(colors)):
         return colors[index]
     return 'black'
+
+def get_avg_roc_auc(results):
+    # aggregate all fpr for all authors
+    all_fpr = np.unique(np.concatenate([result["fpr"] for _, result in results.items()]))
+
+    # Interpolate all ROC curves that these points
+    mean_tpr = np.zeros_like(all_fpr)
+    weighted_mean_tpr = np.zeros_like(all_fpr)
+
+    for author, metrics in results.items():
+        mean_tpr += interp(all_fpr, metrics["fpr"], metrics["tpr"])
+        weighted_mean_tpr += interp(all_fpr, metrics["fpr"], metrics["tpr"]) * metrics[author]["%"]
+
+    # Average out results
+    mean_tpr /= len(results)
+    averages = {
+        "unweighted": {
+            "fpr": all_fpr,
+            "tpr": mean_tpr,
+            "AUC": auc(all_fpr, mean_tpr),
+        },
+        "weighted": {
+            "fpr": all_fpr,
+            "tpr": weighted_mean_tpr,
+            "AUC": auc(all_fpr, weighted_mean_tpr),
+        },
+    }
+    return averages
 
 
 def plot_author_roc_auc_curve(author, fpr, tpr, roc_auc, color='darkorange', directory="classResults", session_name=""):
@@ -38,8 +69,23 @@ def plot_roc_auc_curves(results, directory="classResults", session_name=""):
             directory=directory, session_name=session_name
         )
 
-    # Plot all ROC curves
     lw = 2
+    plt.figure(figsize=(10, 10))
+
+    roc_averages = get_avg_roc_auc(results)
+    # Plot macro average ROC
+    plt.plot(
+        roc_averages["unweighted"]["fpr"], roc_averages["unweighted"]["tpr"], color="deeppink", lw=lw,
+        label="macro average ROC curve (area = {0:0.1f})".format(roc_averages["unweighted"]["AUC"]),
+        linestyle=":",
+    )
+    # Plot weighted average ROC
+    plt.plot(
+        roc_averages["weighted"]["fpr"], roc_averages["weighted"]["tpr"], color="teal", lw=lw,
+        label="weighted average ROC curve (area = {0:0.1f})".format(roc_averages["weighted"]["AUC"]),
+        linestyle=":",
+    )
+    # Plot all ROC curves
     for i, (author, metrics) in enumerate(results.items()):
         plt.plot(
             metrics["fpr"], metrics["tpr"], color=get_color(i), lw=lw,
