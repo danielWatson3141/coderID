abfcaa4473e80b10154d6bb8b128fa7f113e0e93
Dan <danielwatson3141@gmail.com>
2019-08-02 12:45:02 -0400

    Improved Github API integration, improving identity matching and performance.

--- gitProfileSet.py
+++ gitProfileSet.py
@@ -21,6 +21,13 @@ from scipy.sparse import hstack, vstack, csr_matrix
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.feature_selection import mutual_info_classif, SelectKBest
 from ProfileSet import ProfileSet
+import github
+
+def gh():
+    with open(os.getcwd()+"/github.token", 'r') as file:
+        return github.MainClass.Github(file.readline().split("\n")[0], timeout=30)
+        
+
 
 class gitProfileSet:
 
@@ -33,12 +40,14 @@ class gitProfileSet:
         self.name = name
         self.repos = []
         self.authors = dict()
+        self.aliases = dict()
         self.featuresDetected = False
         self.featuresSelected = None
         self.termsSelected = None
         self.minedRepos = set()
         self.commits = set()
-        
+        self.github = gh()
+
     def addRepo(self, args):
         #print("Adding repo: "+args)
         try:
@@ -52,9 +61,9 @@ class gitProfileSet:
         self.repos.remove(arg)
 
     def rmAuthor(self, arg):
-        self.authors[arg] = None  
+        self.authors[arg] = None
     
-    def compileAuthors(self, authors = None):
+    def compileAuthors(self):
         """Mine all repos in the repo list for commits by those in authors. None for get all"""
         print("mining "+self.name)
 
@@ -69,81 +78,84 @@ class gitProfileSet:
                 #print("moved to "+repo)
             if repo in self.minedRepos:
                 continue
-            elif authors is not None:
-                self.minedRepos.add(repo)
+        
+            self.minedRepos.add(repo)
             #if not os.path.exists(repo+".git"):
             #    repo = os.listdir(repo)[0]
+
             miner = pydriller.repository_mining.RepositoryMining(repo, only_modifications_with_file_types=gitProfileSet.langList,only_no_merge=True)
             repository = pydriller.GitRepository(repo)
+            remote = self.get_remote(repo)
             print("Scanning repo: "+miner._path_to_repo)
                         
 
             for commit in tqdm(miner.traverse_commits()):
                 author = commit.author
                 
-                if authors is not None and author not in authors:
-                    continue
+                if author.name not in self.aliases:
+                    ghCommit = remote.get_commit(commit.hash)
+                    namedUser = ghCommit.author
+                    if not namedUser:
+                        continue
+                    if namedUser.login not in self.authors:
+                        self.authors[namedUser.login] = gitAuthor(namedUser)
+                    self.aliases[author.name] = namedUser.login
+
+                author = self.authors[self.aliases[author.name]]
                 
-                if True:    #for now, not worried about commit type
-                    if author.name not in self.authors:
-                        self.authors.update({author.name:gitAuthor(author)})
-                        #print("Found new author: "+author.name)
+                if commit.hash in author.commits or commit.hash in self.commits:
+                    continue    #don't reprocess seen hashes
+
+                self.commits.add(commit.hash)
+                author.commits.add(commit.hash)
+
+                if repo not in author.repos:
+                    author.repos.add(repo)
+                
+                for mod in commit.modifications:
+                    mod._calculate_metrics()
+                    if mod.new_path is None or not mod.new_path.split(".")[-1] in gitProfileSet.langList:
+                        continue
                     
-                    author = self.authors.get(author.name)
+                    author.files.add(mod.new_path)
+                    #parse diff and add lines to list
                     
-                    if commit.hash in author.commits or commit.hash in self.commits:
-                        continue    #don't reprocess seen hashes
+                    newSC = list()
+                    leDiff = repository.parse_diff(mod.diff)
+                    for num, line in leDiff["added"]:
+                        newSC.append(line)
 
-                    self.commits.add(commit.hash)
-                    author.commits.add(commit.hash)
+                    from lizard import analyze_file as liz
+                    fileInfo = liz.analyze_source_code(mod.new_path, "\n".join(newSC))
 
-                    if repo not in author.repos:
-                        author.repos.add(repo)
+                    #maintain list of dicts containing the source code of specific functions. Same format as for lines
+                    lineIndex = 0
                     
-                    for mod in commit.modifications:
-                        mod._calculate_metrics()
-                        if mod.new_path is None or not mod.new_path.split(".")[-1] in gitProfileSet.langList:
-                            continue
+                    for fun in fileInfo.function_list:
+                        #Make sure these appear in the "function"
+                        arg_list_termination = r"\)\s*{"
+                        started = False
+                        newFun = dict()
+                        lineStr = ""
+                        try:
+                            while(leDiff["added"][lineIndex][0]<fun.start_line):
+                                lineIndex+=1
                         
-                        author.files.add(mod.new_path)
-                        #parse diff and add lines to list
-                        
-                        newSC = list()
-                        leDiff = repository.parse_diff(mod.diff)
-                        for num, line in leDiff["added"]:
-                            newSC.append(line)
-
-                        from lizard import analyze_file as liz
-                        fileInfo = liz.analyze_source_code(mod.new_path, "\n".join(newSC))
-
-                        #maintain list of dicts containing the source code of specific functions. Same format as for lines
-                        lineIndex = 0
-                        
-                        for fun in fileInfo.function_list:
-                            #Make sure these appear in the "function"
-                            arg_list_termination = r"\)\s*{"
-                            started = False
-                            newFun = dict()
-                            lineStr = ""
-                            try:
-                                while(leDiff["added"][lineIndex][0]<fun.start_line):
-                                    lineIndex+=1
-                            
-                                while(leDiff["added"][lineIndex][0]<fun.end_line+1):
-                                    last_lineStr = lineStr
-                                    lineStr = leDiff["added"][lineIndex][1]
+                            while(leDiff["added"][lineIndex][0]<fun.end_line+1):
+                                last_lineStr = lineStr
+                                lineStr = leDiff["added"][lineIndex][1]
+                                
+                                if not started and re.search(arg_list_termination, "".join([lineStr,last_lineStr])):
+                                    started = True
                                     
-                                    if not started and re.search(arg_list_termination, "".join([lineStr,last_lineStr])):
-                                        started = True
-                                        
-                                    newFun.update({(commit.hash,mod.new_path,leDiff["added"][lineIndex][0]):lineStr})
-                                    lineIndex+=1
-                            except IndexError: #if end of input reached before end of functions. This is probable when non-complete functions are submitted.
-                                pass
+                                newFun.update({(commit.hash,mod.new_path,leDiff["added"][lineIndex][0]):lineStr})
+                                lineIndex+=1
+                        except IndexError: #if end of input reached before end of functions. This is probable when non-complete functions are submitted.
+                            pass
 
-                            if started and len(newFun) > 1 and '}' in lineStr + last_lineStr:
-                                author.lines.update(newFun)
-                                author.functions.append(self.functionToString(newFun)) 
+                        if started and len(newFun) > 1 and '}' in lineStr + last_lineStr:
+                            author.lines.update(newFun)
+                            author.functions.append(self.functionToString(newFun)) 
                                 
             self.minedRepos.add(repo)
             print(str("finished"+str(miner._path_to_repo)))
@@ -330,7 +342,6 @@ class gitProfileSet:
     def functionToString(self, lines):
         return "\n".join(lines.values())
 
-    
     def __str__(self):
          return (str(len(self.authors))+" authors. "+str(sum(map(lambda x: len(x.functions), self.authors.values())))+" functions in total.")
     
@@ -349,54 +360,44 @@ class gitProfileSet:
                 self.authors[authorName].merge(author)
             else:
                 self.authors[authorName] = author
+    
+    def get_remote(self, localPath):
+        from git import Repo
+
+        local = Repo(localPath)
+        remote = local.remotes[0]
+
+        url = list(remote.urls)[0]
+        if "github.com" not in url:
+            print("not a github url, can't get repo for "+localPath)
+            return None
+
+        import re
+        splUrl = re.split("[./:]", url)
+        fullRepoName = splUrl[5]+"/"+splUrl[6] #get qualified project name from remote url
+
+        return self.github.get_repo(fullRepoName)
+
 
     def fetch_authors_other_work(self):
         """attempts to grab the repos of all authors in the set"""
         sumGPS = gitProfileSet("inverse_"+self.name)
         repoList = []
-        from git import Repo
-
-        authorEmails = set()
-        for authorEmail in self.authors.values():
-            authorEmails.add(authorEmail)
-
-        for repoLocation in self.repos:
-            
-            local = Repo(repoLocation)
-            remote = local.remotes[0]
-
-            url = list(remote.urls)[0]
-            if "github.com" not in url:
-                print("not a github url, can't get contributors for "+repoLocation)
-                continue
-
-            
-            #initialize pygithub object
-            with open(os.getcwd()+"/github.token", 'r') as file:
-                g = github.MainClass.Github(file.readline())
-            while(g.rate_limiting[0]==0):
-                pass
-
-            import re
-            splUrl = re.split("[./:]", url)
-            fullRepoName = splUrl[5]+"/"+splUrl[6] #get qualified project name from remote url
-
-            githubRepo = g.get_repo(fullRepoName)
-            #get Contributors
-            contributors = (githubRepo.get_contributors())
-            
-            for contributor in tqdm(contributors):
-                if contributor.email in authorEmails:               
-                    for repo in contributor.get_repos():
-                        repoList.append(repo.clone_url)
+        
+        for author in tqdm(self.authors.values()):
+            repoList.extend([repo.clone_url for repo in author.getRepos()])
 
         return repoList
 class gitAuthor:
 
-    def __init__(self, dev):
-        if dev is not None:
-            self.name = dev.name
-            self.email = dev.email
+    def __init__(self, namedUser):
+
+        if type(namedUser) is not github.NamedUser.NamedUser:
+            namedUser = gitAuthor.getUserForName(namedUser)
+        
+        self.name = namedUser.login
+        self.email = namedUser.email
+        self.user = namedUser
         self.commits = set() #store with commitHash
         self.files = set()
         self.functions = list() #list of str
@@ -415,38 +416,7 @@ class gitAuthor:
         return self.name+": "+str(len(self.commits))+" commits. "+str(len(self.lines))+" LOC, "+str(len(self.functions))+" complete functions from "+str(len(self.repos))+ " repos."
     
     def getRepos(self, skip=None):
-        """Get all repos associated with a given author, except for those in skip"""
-        with open(os.getcwd()+"/github.token", 'r') as file:
-            g = github.MainClass.Github(file.readline())
-        while(g.rate_limiting[0]==0):
-            pass
-        users = g.search_users(self.email+" in:email")
-        repoList = []
-        try:
-            for user in users:
-                for repo in user.get_repos():
-                    if repo.language == "C++":
-                        repoList.append(repo)
-
-                break #get only first user's stuff
-        except Exception:
-            #print("email not found: "+self.email)
-            pass
-
-        try:
-            if repoList == []:
-                users = g.search_users(self.name+" in:fullname")
-            for user in users:
-                for repo in user.get_repos():
-                    if repo.language == "C++":
-                        repoList.append(repo)
-
-                break #get only first user's stuff
-        except Exception:
-            #print("name not found: "+self.name)
-            pass
-
-        return repoList
+        return self.user.get_repos()
 
     def getRepoListofSelf(self, skipGiven = True, mine = False):
         """Generates a GPS of all repos this author has contributed, skipping existing repos by default."""
@@ -454,6 +424,11 @@ class gitAuthor:
         #print("fetching repos from "+self.name)
         repos = self.getRepos(skip=skipGiven)
         return [repo.clone_url for repo in repos]
+
+    @staticmethod
+    def getUserForName(name):
+        return gh().get_user(name)
+ 
 
 from enum import Enum
 class commitType(Enum):
